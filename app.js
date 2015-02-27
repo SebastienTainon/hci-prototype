1a65db87178e32727c3e8127ad385f833ea32674
Sebastien Tainon <sebastien.tainon@student.ecp.fr>
2015-02-27 15:33:45 +0100

    Add hammerJS, and basic implement of list of articles

--- app.js
+++ app.js
@@ -1,3 +1,8 @@
 $(function() {
+    // https://github.com/Stereobit/dragend
     $( ".draggable" ).draggable();
+
+    $('ul.article-list li').hammer().bind("panleft panright tap",  function(ev) {
+        console.log(ev.type +" gesture detected.");
+    });
 });
